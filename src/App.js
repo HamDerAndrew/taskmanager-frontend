cd5f3f72d1a8a14f3da81135f879d22afecee4b1
André Larsen <andrewsondergaard@gmail.com>
2020-05-26 14:21:19 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the task manager app</h1>
       </header>
+      <div className="login-box">
+        <form>
+          <input type="text" name="username" />
+          <input type="password" name="password" />
+        </form>
+      </div>
     </div>
   );
 }
