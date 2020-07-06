b32f8ce6f29e6162b1a533e36e829cd6204b32cb
André Larsen <andrewsondergaard@gmail.com>
2020-07-06 22:54:46 +0200

    Style: changed text color on Login page

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,21 +5,21 @@ import styles from './navigation.module.css'
 const Navigation = () => {
     return (
         <header className={styles.appHeader}>
-        <nav className={styles.navBar}>
-            <h1>Task Manager App</h1>
-            <ul className={styles.navList}>
-                <li className={styles.navListItem}>
-                <Link to="/">Home</Link>
-                </li>
-                <li className={styles.navListItem}>
-                <Link to="/signup">Sign up</Link>
-                </li>
-                <li className={styles.navListItem}>
-                <Link to="/about">About</Link>
-                </li>
-            </ul>
-        </nav>
-         </header>
+            <nav className={styles.navBar}>
+                <h1>Task Manager App</h1>
+                <ul className={styles.navList}>
+                    <li className={styles.navListItem}>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className={styles.navListItem}>
+                        <Link to="/signup">Sign up</Link>
+                    </li>
+                    <li className={styles.navListItem}>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
